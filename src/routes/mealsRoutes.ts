38c537de666516d2e31d8e18cf054a4feb4d85e2
Cléber Severo <clebersevero1998@gmail.com>
2025-05-27 16:12:09 -0300

    feat: create meal route

--- src/routes/mealsRoutes.ts
+++ src/routes/mealsRoutes.ts
@@ -1,4 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { knex } from "../database";
+import crypto, { randomUUID } from 'node:crypto'
+
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.addHook("preHandler", async (request, reply) => {
@@ -6,6 +10,42 @@ export async function mealsRoutes(app: FastifyInstance) {
   });
 
   app.get("/", async (request, reply) => {
-    return reply.status(201).send("sucesso na sua primeira rota");
+    const meals = await knex('meals').select()
+
+    return reply.status(200).send({ data: meals, message: 'Sucess' });
   });
+
+  app.post('/add', async (request, reply) => {
+    try {
+      const createUserSchema = z.object({
+        user: z.string(),
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        isOnDiet: z.boolean()
+      });
+
+      const { user, date, description, isOnDiet, name } = createUserSchema.parse(request.body);
+
+
+      const userFound = await knex('users').where('id', user).first()
+
+      if (!userFound) {
+        return reply.status(414).send("User not found");
+      }
+
+      await knex('meals').insert({
+        id: crypto.randomUUID(),
+        user_id: userFound.id,
+        description,
+        is_on_diet: isOnDiet,
+        name,
+        datetime: date
+      })
+
+      return reply.status(201).send("Meal added successfully");
+    } catch (error) {
+      console.log(" app.post ~ error:", error)
+    }
+  })
 }
