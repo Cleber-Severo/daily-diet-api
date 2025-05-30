6f88a135429ea47c3007978e2a0084b9468f4937
Cleber Severo <clebersevero1998@gmail.com>
2025-05-29 23:11:08 -0300

    feat: more routes

--- src/routes/mealsRoutes.ts
+++ src/routes/mealsRoutes.ts
@@ -1,8 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { knex } from "../database";
-import crypto, { randomUUID } from 'node:crypto'
-
+import crypto, { randomUUID } from "node:crypto";
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.addHook("preHandler", async (request, reply) => {
@@ -10,82 +9,171 @@ export async function mealsRoutes(app: FastifyInstance) {
   });
 
   app.get("/", async (request, reply) => {
-    const meals = await knex('meals').select()
+    const meals = await knex("meals").select();
 
-    return reply.status(200).send({ data: meals, message: 'Sucess' });
+    return reply.status(200).send({ data: meals, message: "Sucess" });
   });
 
-  app.post('/add', async (request, reply) => {
+  app.get("/:userId", async (request, reply) => {
     try {
-      const createUserSchema = z.object({
-        user: z.string(),
-        name: z.string(),
-        description: z.string(),
-        date: z.string(),
-        isOnDiet: z.boolean()
+      const listUserMealSchema = z.object({
+        userId: z.string().uuid(),
       });
 
-      const { user, date, description, isOnDiet, name } = createUserSchema.parse(request.body);
+      const { userId } = listUserMealSchema.parse(request.params);
 
-
-      const userFound = await knex('users').where('id', user).first()
+      const userFound = await knex("users").where("id", userId).first();
 
       if (!userFound) {
         return reply.status(414).send("User not found");
       }
 
-      await knex('meals').insert({
+      const userMealsList = await knex("meals")
+        .select()
+        .where("user_id", userId);
+
+      return reply.status(200).send({ data: userMealsList });
+    } catch (err) {
+      console.log(err);
+      return reply.status(500).send("Internal error");
+    }
+  });
+
+  app.get("/meal/:mealId", async (request, reply) => {
+    try {
+      const listUserMealSchema = z.object({
+        mealId: z.string().uuid(),
+      });
+
+      const { mealId } = listUserMealSchema.parse(request.params);
+
+      const meal = await knex("meals").select().where("id", mealId).first();
+
+      if (!meal) {
+        return reply.status(414).send("Meals not found");
+      }
+
+      return reply.status(200).send({ data: meal });
+    } catch (err) {
+      console.log(err);
+      return reply.status(500).send("Internal error");
+    }
+  });
+
+  type Meal = {
+    is_on_diet: boolean;
+  };
+
+  app.get("/user-info/:userId", async (request, reply) => {
+    try {
+      const listUserMealSchema = z.object({
+        userId: z.string().uuid(),
+      });
+
+      const { userId } = listUserMealSchema.parse(request.params);
+
+      const userFound = await knex("users").where("id", userId).first();
+
+      if (!userFound) {
+        return reply.status(414).send("User not found");
+      }
+
+      const userMealsList = await knex("meals")
+        .select()
+        .where("user_id", userId);
+
+      const mealsAmount = userMealsList.length;
+      const mealsOnDiet = userMealsList.filter(
+        (item: Meal) => !!item.is_on_diet
+      )?.length;
+      const mealsNotOnDiet = userMealsList.filter(
+        (item: Meal) => !item.is_on_diet
+      )?.length;
+
+      return reply
+        .status(200)
+        .send({ data: { mealsAmount, mealsOnDiet, mealsNotOnDiet } });
+    } catch (err) {
+      console.log(err);
+      return reply.status(500).send("Internal error");
+    }
+  });
+
+  app.post("/add", async (request, reply) => {
+    try {
+      const addMealSchema = z.object({
+        user: z.string(),
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        isOnDiet: z.boolean(),
+      });
+
+      const { user, date, description, isOnDiet, name } = addMealSchema.parse(
+        request.body
+      );
+
+      const userFound = await knex("users").where("id", user).first();
+
+      if (!userFound) {
+        return reply.status(414).send("User not found");
+      }
+
+      await knex("meals").insert({
         id: crypto.randomUUID(),
         user_id: userFound.id,
         description,
         is_on_diet: isOnDiet,
         name,
-        datetime: date
-      })
+        datetime: date,
+      });
 
       return reply.status(201).send("Meal added successfully");
     } catch (error) {
-      console.log(" app.post ~ error:", error)
+      console.log(" app.post ~ error:", error);
     }
-  })
+  });
 
-
-  app.post('/edit', async (request, reply) => {
+  app.post("/edit", async (request, reply) => {
     try {
-      const createUserSchema = z.object({
+      const editMealSchema = z.object({
         mealId: z.string(),
         name: z.string(),
         description: z.string(),
-        isOnDiet: z.boolean()
+        isOnDiet: z.boolean(),
       });
 
-      const { mealId, description, isOnDiet, name } = createUserSchema.parse(request.body);
+      const { mealId, description, isOnDiet, name } = editMealSchema.parse(
+        request.body
+      );
 
-      await knex('meals').update({
-        description,
-        is_on_diet: isOnDiet,
-        name,
-      }).where('id', mealId)
+      await knex("meals")
+        .update({
+          description,
+          is_on_diet: isOnDiet,
+          name,
+        })
+        .where("id", mealId);
 
       return reply.status(201).send("Meal updated");
     } catch (error) {
-      console.log(" app.post ~ error:", error)
+      console.log(" app.post ~ error:", error);
     }
-  })
+  });
 
-  app.post('/delete', async (request, reply) => {
+  app.post("/delete", async (request, reply) => {
     try {
-      const createUserSchema = z.object({
-        mealId: z.string()
+      const deleteMealSchema = z.object({
+        mealId: z.string(),
       });
 
-      const { mealId } = createUserSchema.parse(request.body);
+      const { mealId } = deleteMealSchema.parse(request.body);
 
-      await knex('meals').where('id', mealId).delete()
+      await knex("meals").where("id", mealId).delete();
 
       return reply.status(201).send("Meal has been removed");
     } catch (error) {
-      console.log(" app.post ~ error:", error)
+      console.log(" app.post ~ error:", error);
     }
-  })
+  });
 }
